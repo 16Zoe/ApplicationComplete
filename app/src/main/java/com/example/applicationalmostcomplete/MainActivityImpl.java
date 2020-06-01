59dcac2302958aaa12bc292614f6e86a24f1cd5b
16Zoe <martabolognesitennis@gmail.com>
2020-06-01 18:34:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/applicationalmostcomplete/MainActivityImpl.java
@@ -0,0 +1,172 @@
+package com.example.applicationalmostcomplete;
+
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import com.aldebaran.qi.Future;
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.builder.AnimateBuilder;
+import com.aldebaran.qi.sdk.builder.AnimationBuilder;
+import com.aldebaran.qi.sdk.builder.ChatBuilder;
+import com.aldebaran.qi.sdk.builder.QiChatbotBuilder;
+import com.aldebaran.qi.sdk.builder.TopicBuilder;
+import com.aldebaran.qi.sdk.design.activity.RobotActivity;
+import com.aldebaran.qi.sdk.object.actuation.Animate;
+import com.aldebaran.qi.sdk.object.actuation.Animation;
+import com.aldebaran.qi.sdk.object.conversation.Chat;
+import com.aldebaran.qi.sdk.object.conversation.QiChatVariable;
+import com.aldebaran.qi.sdk.object.conversation.QiChatbot;
+import com.aldebaran.qi.sdk.object.conversation.Topic;
+
+public class MainActivityImpl  extends RobotActivity implements RobotLifecycleCallbacks {
+
+    private static final String TAG ="MainActivty";
+
+    // Textview element in the GUI
+    private TextView textView;
+    private ImageView imageView;
+
+    // The QiContext provided by the QiSDK.
+    private QiContext qiContext = null;
+
+    // Store the Chat action.
+    private Chat chatAction;
+
+    // Animate actions
+    private Animate goodAnimAction, badAnimAction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView1);
+        imageView = findViewById(R.id.imageView);
+        // Register the RobotLifecycleCallbacks to this Activity.
+        QiSDK.register(this, this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        // Unregister the RobotLifecycleCallbacks for this Activity.
+        QiSDK.unregister(this, this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onRobotFocusGained(QiContext qiContext) {
+        Log.i(TAG, "App started.");
+
+        // Store the provided QiContext.
+        this.qiContext = qiContext;
+        initActions();
+
+        // Run the Chat action asynchronously.
+        Future<Void> chatFuture = chatAction.async().run();
+
+        // Add a lambda to the action execution.
+        chatFuture.thenConsume(future -> {
+            if (future.hasError()) {
+                Log.e(TAG, "Discussion finished with error.", future.getError());
+            }
+        });
+
+    }
+
+    @Override
+    public void onRobotFocusLost() {
+        // Remove on started listeners from the Chat action.
+        if (chatAction != null) {
+            chatAction.removeAllOnStartedListeners();
+        }
+    }
+
+    @Override
+    public void onRobotFocusRefused(String reason) {
+        // The robot focus is refused.
+    }
+
+    private void textViewSetValue(String val) {
+        runOnUiThread(() -> textView.setText(val));
+    }
+
+    private void imageViewSetValue(String val) {
+        if (val.equals("juice"))
+            runOnUiThread(() -> imageView.setImageResource(R.drawable.juice));
+
+    }
+
+    public void initActions() {
+        // Create a chat topic
+        Topic topic = TopicBuilder.with(qiContext) // Create the builder using the QiContext.
+                .withResource(R.raw.greetings) // Set the topic resource.
+                .build(); // Build the topic.
+        // Create a new QiChatbot.
+        QiChatbot qiChatbot = QiChatbotBuilder.with(qiContext)
+                .withTopic(topic)
+                .build();
+        // Create a new Chat action.
+        chatAction = ChatBuilder.with(qiContext)
+                .withChatbot(qiChatbot)
+                .build();
+
+        // Add an on started listener to the Chat action.
+        chatAction.addOnStartedListener(() -> Log.i(TAG, "Discussion started."));
+
+
+        // Set up a listener for a chat variable
+        QiChatVariable nameVariable = qiChatbot.variable("Name");
+
+        nameVariable.addOnValueChangedListener(
+                currentValue -> {
+                    Log.i(TAG, "Chat var Name: " + currentValue);
+                    textViewSetValue("Hello " + currentValue);
+                }
+        );
+
+        // Set up a listener for a chat variable
+        QiChatVariable drinkVariable = qiChatbot.variable("Drink");
+
+        drinkVariable.addOnValueChangedListener(
+                currentValue -> {
+                    Log.i(TAG, "Chat var Drink: " + currentValue);
+                    imageViewSetValue(currentValue);
+                }
+        );
+
+
+        // Create animations
+        Animation animation_good = AnimationBuilder.with(qiContext) // Create the builder with the context.
+                .withResources(R.raw.nicereaction_a001) // Set the animation resource.
+                .build(); // Build the animation.
+        Animation animation_bad = AnimationBuilder.with(qiContext) // Create the builder with the context.
+                .withResources(R.raw.sad_a003) // Set the animation resource.
+                .build(); // Build the animation.
+
+        // Create animate actions
+        goodAnimAction = AnimateBuilder.with(qiContext) // Create the builder with the context.
+                .withAnimation(animation_good) // Set the animation.
+                .build(); // Build the animate action.
+
+        badAnimAction = AnimateBuilder.with(qiContext) // Create the builder with the context.
+                .withAnimation(animation_bad) // Set the animation.
+                .build(); // Build the animate action.
+
+        qiChatbot.addOnBookmarkReachedListener((bookmark) -> {
+            Log.i(TAG, "Bookmark event: " + bookmark.getName());
+            if (bookmark.getName().equals("feelgood")) {
+                goodAnimAction.async().run();
+            }
+            else if (bookmark.getName().equals("feelbad")) {
+                badAnimAction.async().run();
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/com/example/applicationalmostcomplete/MainActivityTest.java
@@ -0,0 +1,89 @@
+package com.example.applicationalmostcomplete;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.builder.AnimateBuilder;
+import com.aldebaran.qi.sdk.builder.AnimationBuilder;
+import com.aldebaran.qi.sdk.builder.SayBuilder;
+import com.aldebaran.qi.sdk.design.activity.RobotActivity;
+import com.aldebaran.qi.sdk.object.actuation.Animate;
+import com.aldebaran.qi.sdk.object.actuation.Animation;
+import com.aldebaran.qi.sdk.object.conversation.Say;
+
+public class MainActivityTest  extends RobotActivity implements RobotLifecycleCallbacks {
+
+    // GUI button
+    private Button button1;
+    // The QiContext provided by the QiSDK.
+    private QiContext qiContext = null;
+
+    private Say sayAction1, sayAction2;
+    private Animate helloAction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = findViewById(R.id.button1);
+        // Set the button onClick listener.
+        button1.setOnClickListener(v -> {
+            sayAction2.async().run();
+            helloAction.async().run();
+        });
+
+        // Register the RobotLifecycleCallbacks to this Activity.
+        QiSDK.register(this, this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        // Unregister the RobotLifecycleCallbacks for this Activity.
+        QiSDK.unregister(this, this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onRobotFocusGained(QiContext qiContext) {
+        // Store the provided QiContext.
+        this.qiContext = qiContext;
+        initActions();
+        sayAction1.async().run();
+    }
+
+    @Override
+    public void onRobotFocusLost() {
+        // Remove the QiContext.
+        this.qiContext = null;
+    }
+
+    @Override
+    public void onRobotFocusRefused(String reason) {
+        // The robot focus is refused.
+    }
+
+    public void initActions() {
+        // Create new say actions.
+        sayAction1 = SayBuilder.with(qiContext) // Create the builder with the context.
+                .withText("Hello there!") // Set the text to say.
+                .build(); // Build the say action.
+
+        sayAction2 = SayBuilder.with(qiContext) // Create the builder with the context.
+                .withText("OK, let's start!") // Set the text to say.
+                .build(); // Build the say action.
+
+        // Create an animation.
+        Animation animation = AnimationBuilder.with(qiContext) // Create the builder with the context.
+                .withResources(R.raw.hello_a003) // Set the animation resource.
+                .build(); // Build the animation.
+
+        // Create an animate action.
+        helloAction = AnimateBuilder.with(qiContext) // Create the builder with the context.
+                .withAnimation(animation) // Set the animation.
+                .build(); // Build the animate action.
+    }
+
+}
